4564edf9e9b2c168ad189cd9be747153865e3c92
Brian Lambert <brianlambert2408@gmail.com>
2022-11-08 17:15:30 +1100

    Added clickable images

--- src/components/dash-components/details-components/DetailsImage.jsx
+++ src/components/dash-components/details-components/DetailsImage.jsx
@@ -1,53 +1,70 @@
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+
 export default function DetailsImage({imageData, imageName}){
 
     //main image
     const { front_default:main } = imageData.other["official-artwork"];
 
-    //small images obtained by filtering
-    const extraImgs = Object.values(imageData)
-    .filter(data=>typeof data === 'string') // finds only strings and removes nested objects
-    .filter(data=> !data.includes('shiny')) // removes shinies
-    .map(data=>{ //creates an object that assigns it a gender and position
-        let gender = "male";
-        let pose = "front";
+    const [mainImage, setMainImage ] = useState(main);
+    const [allImages, setAllImages] = useState([]);
 
-        if(data.includes("female")){
-            gender=  "female";
-        }
+    useEffect(()=>{
+        //small images obtained by filtering
+        const extraImgs = Object.values(imageData)
+        .filter(data=>typeof data === 'string') // finds only strings and removes nested objects
+        .filter(data=> !data.includes('shiny')) // removes shinies
+        .map(data=>{ //creates an object that assigns it a gender and position
+            let gender = "male";
+            let pose = "front";
 
-        if(data.includes("back")){
-            pose = "back";
-        }
+            if(data.includes("female")){
+                gender=  "female";
+            }
 
-        return {url: data, gender, pose};
-    })
-    .sort((a, b)=>{ //sort by gender
-        if(a.gender > b.gender){
-            return -1;
-        }
-        if(a.gender < b.gender){
-            return 1;
-        }
-        return 0;
-    })
-    .map(({url, pose})=>{
+            if(data.includes("back")){
+                pose = "back";
+            }
 
-        const altString = `${imageName}${(pose !=="none" ? ` | ${pose}` : "")}`;
+            return {url: data, gender, pose};
+        })
+        .sort((a, b)=>{ //sort by gender
+            if(a.gender > b.gender){
+                return -1;
+            }
+            if(a.gender < b.gender){
+                return 1;
+            }
+            return 0;
+        })
+        .map(({url, pose})=>{
 
-        return <img 
+            const altString = `${imageName}${(pose !=="none" ? ` | ${pose}` : "")}`;
+
+            const finalImage = <img 
             key={url} 
             src={url} 
             alt={altString} 
-        />
-    }); //final image JSX array
+            />
+
+            return url
+        }); //final image JSX array
+
+        setAllImages(imgs=>[main, ...extraImgs])
+        //console.log(allImages)
+    },[]);
+
+    
 
     return (
         <div className="section-image">
-            <div>
-                <img src={main} alt={`${imageName} | official art`} />
+            <div style={{height: "300px", width: "100%", display:"flex", justifyContent:"center"}}>
+                <img style={{maxHeight: "100%", maxWidth:"100%"}} src={mainImage} alt={`${imageName} | official art`} />
             </div>
             <div>
-                {extraImgs}
+                {allImages.map((i)=>{
+                    return <img style={{maxHeight:"50px", maxWidth:"100%"}} key={uuid()} src={i} onClick={()=>setMainImage(img=>i)}/>
+                })}
             </div>
             
         </div>
